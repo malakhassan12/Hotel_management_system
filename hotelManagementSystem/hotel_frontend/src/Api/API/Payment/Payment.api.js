ace1fa3d7734ece5e0e66005014bbd0569904f53
malakhassan12 <158052165+malakhassan12@users.noreply.github.com>
2026-05-08 00:25:19 +0300

    Malak send agin

--- hotelManagementSystem/hotel_frontend/src/Api/API/Payment/Payment.api.js
+++ hotelManagementSystem/hotel_frontend/src/Api/API/Payment/Payment.api.js
@@ -48,41 +48,21 @@ const makeConfirm = async ({
   return res?.data;
 };
 
+
+
 // const stripeConfirm = async (clientSecret) => {
-//   const stripeIntentId = clientSecret.split("_secret_")[0];
+//   const paymentIntentId = clientSecret.split("_secret_")[0];
 
-//   const stripeResponse = await axios.post(
-//     `https://api.stripe.com/v1/payment_intents/${stripeIntentId}/confirm`,
-//     new URLSearchParams({
-//       "payment_method_data[type]": "card",
-//       "payment_method_data[card][token]": "tok_visa",
-//       return_url: "https://example.com",
-//     }),
-//     {
-//       headers: {
-//         Authorization: `Bearer sk_test_51TUT4XJc5LM0oFyF2wHzQRKYwbOEBp9L3hFDEgD14KKpcaMwJ3j6bnikGTxZjMoq8XYnF4D6emrIGkxDgl6kKoAu00orMib8Pd`,
-//         "Content-Type": "application/x-www-form-urlencoded",
-//       },
+//   const res = await paymentClient.post("/confirm", null, {
+//     params: {
+//       paymentIntentId,
+//       customerName: "test",
+//       customerEmail: "test@gmail.com",
+//       customerPhone: "01000000000",
 //     },
-//   );
+//   });
 
-//   console.log(stripeResponse);
+//   return res.data;
 // };
 
-
-const stripeConfirm = async (clientSecret) => {
-  const paymentIntentId = clientSecret.split("_secret_")[0];
-
-  const res = await paymentClient.post("/confirm", null, {
-    params: {
-      paymentIntentId,
-      customerName: "test",
-      customerEmail: "test@gmail.com",
-      customerPhone: "01000000000",
-    },
-  });
-
-  return res.data;
-};
-
-export { getPendingPayment, createPayment, makeConfirm, stripeConfirm };
+export { getPendingPayment, createPayment, makeConfirm,  };
